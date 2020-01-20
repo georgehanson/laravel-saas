fd3742afa06099dd22562f0c0d24c530f5efc9c3
George Hanson <george@alisons-imac-9.home>
2020-01-20 13:01:06 +0000

    Update handler

--- src/Services/Webhooks/InvoicePaymentSucceeded.php
+++ src/Services/Webhooks/InvoicePaymentSucceeded.php
@@ -17,6 +17,6 @@ class InvoicePaymentSucceeded implements Handler
     public function process(Event $event)
     {
        // The invoice payment was successful. We need to mark the tenants subscription as active and set the expiry date.
-        Log::error($event->data);
+        Log::error($event->data->lines->data[0]->plan);
     }
 }
